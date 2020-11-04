1eacbbe0fdb54495b70198d1ca24119a18846091
An <annguyen@Ans-MacBook-Pro.local>
2020-11-04 00:11:42 -0500

    Hand skeleton

---
+++ Hand.cpp
@@ -0,0 +1,17 @@
+#include "Hand.h"
+
+using namespace std;
+
+Hand::Hand() {}
+
+Hand::~Hand() {}
+
+vector<Card> getCards() {}
+
+int getTotalValue() {}
+
+void addCard() {}
+
+bool checkBust() {}
+
+bool checkBlackjack() {}
---
+++ Hand.h
@@ -0,0 +1,21 @@
+#ifndef HAND_H
+#define HAND_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Hand {
+    private:
+        std::vector<Card> cards;
+        int totalValue;
+    
+    public:
+        Hand();
+        ~Hand();
+        std::vector<Card> getCards();
+        int getTotalValue();
+        void addCard();
+        bool checkBust();
+        bool checkBlackjack();
+}
