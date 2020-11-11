4feaea7d95942299fecfd86885c3722440433da7
Bridget Hart <44095906+bhart4@users.noreply.github.com>
2020-11-11 14:10:25 -0500

    Removing comments

--- models/Human.cpp
+++ models/Human.cpp
@@ -5,15 +5,11 @@
 
 using namespace std;
 
-// include hand class
-
 Human::Human() {
     sum = 0;
     vector<Hand> hand(0, 0);
 }
 
-// if dealt 2 cards w same value, option to create another hand and add one card to each, 
-// then can ask for new second card for each  
 void splitHand(vector<Card> card1, vector<Card> card2) { 
     if card1.number == card2.number {
         vector<Hand> hand2(card2, card2.number);
