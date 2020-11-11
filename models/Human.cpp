38c79aeab2350812367a69ee52e303df17996134
Bridget Hart <44095906+bhart4@users.noreply.github.com>
2020-11-11 14:08:23 -0500

    human and dealer implementation

--- models/Human.cpp
+++ models/Human.cpp
@@ -1,9 +1,23 @@
 #include "Human.h"
+#include "Hand.h"
+#include "Card.h"
+#include <vector>
 
 using namespace std;
 
-Human::Human() {}
+// include hand class
 
-void splitHand() {}
+Human::Human() {
+    sum = 0;
+    vector<Hand> hand(0, 0);
+}
+
+// if dealt 2 cards w same value, option to create another hand and add one card to each, 
+// then can ask for new second card for each  
+void splitHand(vector<Card> card1, vector<Card> card2) { 
+    if card1.number == card2.number {
+        vector<Hand> hand2(card2, card2.number);
+    }
+}
 
 Human::~Human() {}
